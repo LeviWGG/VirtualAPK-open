fc333143bf93adada98eb8f44d2c8debe89ae89e
LeviWGG <yugiop@126.com>
2018-02-24 21:59:06 +0800

    add arr

--- PluginDemo/app/src/main/java/app/main/wangliwei/plugindemo/PluginActivity.java
+++ PluginDemo/app/src/main/java/app/main/wangliwei/plugindemo/PluginActivity.java
@@ -4,21 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class PluginActivity extends Activity {
+    private Unbinder unbinder;
+
+    @BindView(R.id.text_back_host)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plugin2);
+        unbinder = ButterKnife.bind(this);
 
-        findViewById(R.id.text_back_host).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClassName("app.main.wangliwei.hostdemo", "app.main.wangliwei.hostdemo.MainActivity");
-                startActivity(intent);
-            }
-        });
+//        findViewById(R.id.text_back_host).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent();
+//                intent.setClassName("app.main.wangliwei.hostdemo", "app.main.wangliwei.hostdemo.MainActivity");
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+    @OnClick(R.id.text_back_host)
+    public void onClick() {
+        Intent intent = new Intent();
+        intent.setClassName("app.main.wangliwei.hostdemo", "app.main.wangliwei.hostdemo.MainActivity");
+        startActivity(intent);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # VirtualAPK-open
 这是一个基于VirtualAPK的插件式框架
+
+![demo](http://upload-images.jianshu.io/upload_images/5256137-d372dacca0a9e93d.gif?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
